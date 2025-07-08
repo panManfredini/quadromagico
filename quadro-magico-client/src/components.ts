618826a3b3608ae480895ac7f59043695336aefe
panManfredini <pan.manfredini@gmail.com>
2025-07-08 23:39:53 +0200

    tabs

--- quadro-magico-client/src/components.ts
+++ quadro-magico-client/src/components.ts
@@ -1,8 +1,8 @@
 import '@shoelace-style/shoelace/dist/themes/dark.css';
 import '@shoelace-style/shoelace/dist/components/button/button.js';
-import '@shoelace-style/shoelace/dist/components/icon/icon.js';
-import '@shoelace-style/shoelace/dist/components/input/input.js';
-import '@shoelace-style/shoelace/dist/components/rating/rating.js';
+import '@shoelace-style/shoelace/dist/components/tab-group/tab-group.js'
+import '@shoelace-style/shoelace/dist/components/tab/tab.js'
+import '@shoelace-style/shoelace/dist/components/tab-panel/tab-panel.js'
 
 import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
 setBasePath('/');
